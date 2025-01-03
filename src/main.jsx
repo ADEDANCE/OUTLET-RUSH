ba3cf268938b352932343ffbf7fc5076a4c8d4b1
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-01-03 18:09:35 +0100

    import App file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../src/style/style.css'
-import Acconts from './welcome-paages/Acconts.jsx'
-import Wmessage from './welcome-paages/Wmessage.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Acconts /> */}
-    <Wmessage />
+    <App />
   </StrictMode>,
 )
