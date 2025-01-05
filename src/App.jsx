96cee5f9fd76ec2ea376ac233f5eb5939521b121
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-01-05 06:52:35 +0100

    Add SignupBuyer path

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Wmessage from './welcome-paages/Wmessage';
-import Acconts from './welcome-paages/Acconts';
+import SignupBuyer from './welcome-paages/SignUp/SignupBuyer';
+import SignupSeller from './welcome-paages/SignUp/SignupSeller'
+
 
 const App = () => {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<Wmessage />} />
-      <Route path="/Acconts" element={<Acconts />} />
+      <Route path="/SignupBuyer" element={<SignupBuyer />} />
+      <Route path="/SignupSeller" element={<SignupSeller/>} />
     </Routes>
   </Router>
   )
