6b3091d756b9d7dc6fafba853f98b9330345ccd2
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-01-06 05:34:47 +0100

    import react-bootsrap

--- src/welcome-paages/SignUp/SignupBuyer.jsx
+++ src/welcome-paages/SignUp/SignupBuyer.jsx
@@ -1,188 +1,253 @@
-import React from 'react'
-import  {useState} from 'react'
-import {Container, Card} from 'react-bootstrap'
-import {useNavigate} from 'react-router-dom'
+import React, { useState } from "react";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {FaEye,FaEyeSlash} from "react-icons/fa"
 
 const SignupBuyer = () => {
- const [formData, setFormData] = useState({
-  name: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-  role: "Minor", 
-  bvn: "",
-  description: "",
-  age: "",
-  gender: "",
-  phone: "",
- });
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    bvn: "",
+    gender: "",
+    Phone: "",
+  });
 
-const [passwordError, setPasswordError] = useState("");
- const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const navigate = useNavigate();
 
- const handleChange = (e) => {
-  const { name, value } = e.target;
-  setFormData((prev) => ({ ...prev, [name]: value }));
-};
+  const initialValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    bvn: "",
+    gender: "",
+    Phone: "",
+  };
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  e.preventDefault();
-
-  // Validate password length
-  if (formData.password.length < 4) {
-    setPasswordError("Password must be at least 4 characters long.");
-    valid = false;
-  } else {
-    setPasswordError(""); 
-  }
+  const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
 
-  if (formData.password !== formData.confirmPassword) {
-    setConfirmPasswordError("Passwords do not match!");
-    valid = false;
-  } else {
-    setConfirmPasswordError(""); 
-  }
+  const validationSchema = Yup.object({
+    first_name: Yup.string().required("First name is required"),
+    last_name: Yup.string().required("Last name is required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    bvn: Yup.string().required("bvn is required"),
+    gender: Yup.string().required("Input your gender"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Required"),
+      confirm_password: Yup.string()
+      .oneOf([Yup.ref("password")], "Passwords do not match")
+      .required("Please confirm password."),
+      Phone: Yup.string()
+      .matches(
+        /^((\+234)|(0))\d{10}$/,
+        "Phone number must start with +234 or 0 and contain exactly 11 digits"
+      )
+      .length(11, "Phone number must be exactly 11 digits") 
+      .required("Phone number is required"),
+  });
+  
 
-  if (valid) {
-  localStorage.setItem("userData", JSON.stringify(formData));
-  alert("Sign-up successful!");
-  }
- 
-  handleHomeButton();
-};
+  const handleSubmit = (values, { resetForm }) => {
+    setFormData(values); // Update state with form data
+    resetForm(false); // Reset the form
+    navigate("/age"); // Navigate to another page
+  };
 
-const navigate = useNavigate();
-const handleHomeButton = () => {
-  navigate("/"); 
-};
+  const handleHomeButton = () => {
+    navigate("/");
+  };
 
   return (
-         <Container fluid>
-               <div className="CreateA-bg">
-                   <div>
-                       <h2>Create an account</h2>
-                   </div>
-                  <Card className="signup-card "> 
-                       <form onSubmit={handleSubmit}>
-                            <div className='signup-inputs'>
-                                <input
-                                   type="text"
-                                   name="firstName"
-                                   placeholder="First Name"
-                                   value={formData.firstName}
-                                   onChange={handleChange}
-                                   required
-                               />
-                               <input
-                                    type="text"
-                                    name="lastName"
-                                    placeholder="Last Name"
-                                    value={formData.lastName}
-                                    onChange={handleChange}
-                                    required
-                              />
-             <input
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-             <input
-              type="text"
-              name="userName"
-              placeholder="User Name"
-              value={formData.userName}
-              onChange={handleChange}
-              required
-            />
-           <input
-                    type="text"
-                    name="bvn"
-                    placeholder="BVN"
-                    value={formData.bvn}
-                    onChange={handleChange}
-                    required
-               />
-              <input 
-            type="text"
-            name='age'
-            placeholder='Age'
-            value={formData.age}
-            onChange={handleChange}
-            required
-             />
-              <input 
-            type="text"
-            name='gender'
-            placeholder='Gender'
-            value={formData.bvn}
-            onChange={handleChange}
-            required
-             />
-              <input 
-            type="text"
-            name='phone'
-            placeholder='Phone'
-            value={formData.phone}
-            onChange={handleChange}
-            required
-             />
-
-<input
-              type="text"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-             {passwordError && <p style={{ color: "red", fontSize: "0.9rem" }}>{passwordError}</p>}
-             <input
-              type="text"
-              name="confirmPassword"
-              placeholder="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              required
-            />
-             {confirmPasswordError && <p style={{ color: "red", fontSize: "0.9rem" }}>{confirmPasswordError}</p>}
-
-            </div>
-            {/* <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </div> */}
-            {/* <div>
-        <label>Confirm Password:</label>
-        <input
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required
-        />
-      </div> */}
-    </form>
-       <div>
-       <button type="submit" onClick={handleSubmit}>Sign Up</button>
-       <button type="reset" onClick={handleHomeButton}>back to home page</button>
-       </div>
-    </Card>
-    </div>
+    <Container fluid>
+      <div className="CreateA-bg">
+        <h2>Create an account</h2>
+        <Card className="signup-card">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <Row>
+                  <Col md={6} className="mt-3">
+                    <Field
+                      type="text"
+                      name="first_name"
+                      placeholder="First Name *"
+                      className={`input-field ${
+                        touched.first_name && errors.first_name ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="first_name"
+                      component="div"
+                      className="invalid-feedback"
+                      style={{ color: "red" }}
+                    />
+                  </Col>
+                  <Col md={6} className="mt-3">
+                    <Field
+                      type="text"
+                      name="last_name"
+                      placeholder="Last Name *"
+                      className={`input-field ${
+                        touched.last_name && errors.last_name ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="last_name"
+                      component="div"
+                      className="invalid-feedback"
+                      style={{ color: "red" }}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={6} className="mt-3">
+                    <Field
+                      type="email"
+                      name="email"
+                      placeholder="Email Address *"
+                      className={`input-field ${
+                        touched.email && errors.email ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="invalid-feedback"
+                      style={{ color: "red" }}
+                    />
+                  </Col>
+                  <Col md={6} className="mt-3">
+                    <Field
+                      type="number"
+                      name="Phone"
+                      placeholder="Phone *"
+                      className={`input-field ${
+                        touched.Phone && errors.Phone? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="Phone"
+                      component="div"
+                      className="invalid-feedback"
+                      style={{ color: "red" }}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={6} className="mt-3">
+                    <Field
+                      type="text"
+                      name="bvn"
+                      placeholder="BVN*"
+                      className={`input-field ${
+                        touched.bvn && errors.bvn ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="bvn"
+                      component="div"
+                      className="invalid-feedback"
+                      style={{ color: "red" }}
+                    />
+                  </Col>
+                  <Col md={6} className="mt-3">
+                    <Field
+                      type="text"
+                      name="gender"
+                      placeholder="Gender*"
+                      className={`input-field ${
+                        touched.gender && errors.gender ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="gender"
+                      component="div"
+                      className="invalid-feedback"
+                      style={{ color: "red" }}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                                        <Col md={6} className="mt-3">
+                                            <div className="position-relative">
+                                                <Field
+                                                    type={showPassword ? "text" : "password"}
+                                                    name="password"
+                                                    placeholder="Password *"
+                                                    className={`input-field ${touched.password && errors.password
+                                                        ? "is-invalid"
+                                                        : ""
+                                                        }`}
+                                                />
+                                                <ErrorMessage name="password" component="div" className="invalid-feedback" />
+                                                <span
+                                                    className="password-toggle-icon"
+                                                    onClick={() => setShowPassword(!showPassword)}
+                                                    style={{
+                                                        position: 'absolute',
+                                                        right: '10px',
+                                                        top: '50%',
+                                                        transform: 'translateY(-50%)',
+                                                        cursor: 'pointer',
+                                                    }}
+                                                >
+                                                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                                                </span>
+                                            </div>
+                                        </Col>
+                                        <Col md={6} className="mt-3">
+                                            <div className="position-relative">
+                                                <Field
+                                                    type={showConfirmPassword ? "text" : "password"}
+                                                    name="confirm_password"
+                                                    placeholder="Confirm Password *"
+                                                    className={`input-field ${touched.confirm_password && errors.confirm_password
+                                                        ? "is-invalid"
+                                                        : ""
+                                                        }`}
+                                                />
+                                                <ErrorMessage name="confirm_password" component="div" className="invalid-feedback" />
+                                                <span
+                                                    className="password-toggle-icon"
+                                                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                                    style={{
+                                                        position: 'absolute',
+                                                        right: '10px',
+                                                        top: '50%',
+                                                        transform: 'translateY(-50%)',
+                                                        cursor: 'pointer',
+                                                    }}
+                                                >
+                                                    {showConfirmPassword ? <FaEye /> : <FaEyeSlash />}
+                                                </span>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                <button type="submit">Sign Up</button>
+                <button type="button" onClick={handleHomeButton}>
+                  Back to Home
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </Card>
+      </div>
     </Container>
-   
-    
-  )
-}
+  );
+};
 
-export default SignupBuyer
+export default SignupBuyer;
