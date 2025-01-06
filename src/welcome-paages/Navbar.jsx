50a72a4217b27931851740ad25173035d317b5da
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-01-06 05:33:42 +0100

    fix some bugs

--- src/welcome-paages/Navbar.jsx
+++ src/welcome-paages/Navbar.jsx
@@ -7,3 +7,32 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+
+ //const handleSubmit = (e) => {
+    //   e.preventDefault();
+      
+    
+    //   // Validate password length
+    //   // if (formData.password.length < 4) {
+    //   //   setPasswordError("Password must be at least 4 characters long.");
+    //   // 
+    //   // } else {
+    //   //   setPasswordError(""); 
+    //   // }
+    
+    
+    //   // if (formData.password !== formData.confirmPassword) {
+    //   //   setConfirmPasswordError("Passwords do not match!");
+    //   //   valid = false;
+    //   // } else {
+    //   //   setConfirmPasswordError(""); 
+    //   // }
+    
+    //   // if (valid) {
+    //   // localStorage.setItem("userData", JSON.stringify(formData));
+    //   // alert("Sign-up successful!");
+    //   // }
+     
+    // };
