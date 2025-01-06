2493fa1feb59c0d6fc9dd498284d4cc93905c34a
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-01-06 12:59:56 +0100

    Create new folder

---
+++ src/welcome-paages/Profile-pages/SellerProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SellerProfile = () => {
+  return (
+    <div>SellerProfile
+
+      <h1>
+        You self no smalll
+      </h1>
+    </div>
+  )
+}
+
+export default SellerProfile
