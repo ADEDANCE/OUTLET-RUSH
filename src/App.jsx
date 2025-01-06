62c6cba17ec7ddaf1c0007f5f1e9f452377676f3
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-01-06 13:04:11 +0100

    Add the new file Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Wmessage from './welcome-paages/Wmessage';
 import SignupBuyer from './welcome-paages/SignUp/SignupBuyer';
-import SignupSeller from './welcome-paages/SignUp/SignupSeller'
+import SignupSeller from './welcome-paages/SignUp/SignupSeller';
+import SellerProfile from './welcome-paages/Profile-pages/SellerProfile';
 
 
 const App = () => {
@@ -12,6 +13,18 @@ const App = () => {
       <Route path="/" element={<Wmessage />} />
       <Route path="/SignupBuyer" element={<SignupBuyer />} />
       <Route path="/SignupSeller" element={<SignupSeller/>} />
+      <Route path='/SellerProfile' element={<SellerProfile/>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    } />
     </Routes>
   </Router>
   )
