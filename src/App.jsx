39db1bced46f9a5d66b3852a70e858b75607d3f7
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-02-08 18:28:55 +0100

    Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import SignupBuyer from './welcome-paages/SignUp/SignupBuyer';
 import SignupSeller from './welcome-paages/SignUp/SignupSeller';
 import SellerProfile from './SELLER-PAGES/SellerProfile';
 import ProductView from './SELLER-PAGES/ProductView';
+import Seller from './NAVBARS/Seller';
+import ManageOrder from './SELLER-PAGES/ManageOrder';
+import Earnings from './SELLER-PAGES/Earnings';
 
 
 const App = () => {
@@ -14,8 +17,13 @@ const App = () => {
       <Route path="/" element={<Wmessage />} />
       <Route path="/SignupBuyer" element={<SignupBuyer />} />
       <Route path="/SignupSeller" element={<SignupSeller/>} />
-      <Route path='/SellerProfile' element={<SellerProfile/>}/>
-      <Route path="/ProductView" element={<ProductView />} />
+     
+      <Route path="/Seller" element={<Seller />}>
+          <Route index element={<SellerProfile />} /> {/* Default page inside Seller */}
+          <Route path="ManageOrder" element={<ManageOrder />} />  
+          <Route path="ProductView" element={<ProductView />} />
+          <Route path='Earnings' element={<Earnings />} />
+        </Route>
       
     </Routes>
   </Router>
